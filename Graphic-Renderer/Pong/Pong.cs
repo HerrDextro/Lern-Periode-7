41a382aeb6369dfe2cac70599eb196bf7db6781d
Neo <Neo@DESKTOP-OV7RN3A>
2024-11-08 14:53:26 +0100

    .

---
+++ Graphic-Renderer/Pong/Pong.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Text;
+
+namespace Graphic_Renderer
+{
+    public class Pong
+    {
+        public void StartGame(SPainter painterInp)
+        {
+            Console.WriteLine("test success");
+        }
+    }
+}
