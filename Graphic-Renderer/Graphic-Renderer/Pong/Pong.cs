5fc1e27fde96831bff770204fcdbd47d2f41f906
Neo <Neo@DESKTOP-OV7RN3A>
2024-11-08 09:10:03 +0100

    starting pong, finally!

--- Graphic-Renderer/Graphic-Renderer/Pong/Pong.cs
+++ Graphic-Renderer/Graphic-Renderer/Pong/Pong.cs
@@ -7,36 +7,7 @@ namespace Graphic_Renderer
     {
         public void StartGame(SPainter painter)
         {
-            painter.clear();
-            painter.fillRectangle("white", 0, 0, 7, 6);
-
-            int[,] arr =
-            {
-                {0, 0, 1, 1, 1, 0, 0},
-                {0, 1, 1, 0, 1, 1, 0},
-                {1, 1, 0, 1, 0, 1, 1},
-                {0, 1, 1, 1, 1, 1, 0},
-                {0, 1, 0, 1, 0, 1, 0},
-                {0, 1, 0, 1, 0, 1, 0}
-            };
-
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    if (arr[i, j] == 0)
-                    {
-                        painter.changePixel("black", j, i);
-                    }
-
-                }
-            }
-
-            painter.saveImage(0, 0, 7, 6, "C:\\Users\\alex\\Source\\Repos\\HerrDextro\\Lern-Periode-7\\Graphic-Renderer\\Graphic-Renderer\\SpaceInvaders\\textures\\enemy04HIT.txt");
-
-
-            painter.updateFrame();
-            Thread.Sleep(10000);
+          //comment
         }
     }
 }
