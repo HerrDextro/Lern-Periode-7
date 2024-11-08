6e30d5d1dfb7c7eaeecd92ce40a119e7fbdff87b
Neo <Neo@DESKTOP-OV7RN3A>
2024-11-08 08:46:47 +0100

    .

--- Pong/PongV8.cs
+++ Pong/PongV8.cs
@@ -1,33 +1,42 @@
-using System;
-using System.Data;
-using System.Runtime.InteropServices;
-using System.Threading;
-using Graphic_Renderer;
+using System.Reflection;
+using System.Text;
 
 namespace Graphic_Renderer
 {
     public class Pong
     {
-        public void StartGame()
+        public void StartGame(SPainter painter)
         {
-            SPainter painter = new SPainter(60, 30, "black");
-            Console.Clear();
-            painter.renderFrame();
+            painter.clear();
+            painter.fillRectangle("white", 0, 0, 7, 6);
 
-            painter.fillRectangle("red", 2, 2, 10, 10);
+            int[,] arr =
+            {
+                {0, 0, 1, 1, 1, 0, 0},
+                {0, 1, 1, 0, 1, 1, 0},
+                {1, 1, 0, 1, 0, 1, 1},
+                {0, 1, 1, 1, 1, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0}
+            };
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] == 0)
+                    {
+                        painter.changePixel("black", j, i);
+                    }
+
+                }
+            }
+
+            painter.saveImage(0, 0, 7, 6, "C:\\Users\\alex\\Source\\Repos\\HerrDextro\\Lern-Periode-7\\Graphic-Renderer\\Graphic-Renderer\\SpaceInvaders\\textures\\enemy04HIT.txt");
+
 
             painter.updateFrame();
-
-            Thread.Sleep(5000);
-
-            //how to make pong brainstorm
-            //define field sizes
-            //const in fieldX
-            //const int fieldY
-
-            //define paddle
-
+            Thread.Sleep(10000);
         }
     }
 }
-
