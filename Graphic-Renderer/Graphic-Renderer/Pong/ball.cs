b1f3d55053a65240880b82d38f47f58d80b0bf8b
Neo <Neo@DESKTOP-OV7RN3A>
2024-11-22 10:43:28 +0100

    made ballmovements and field constraints

--- Graphic-Renderer/Graphic-Renderer/Pong/ball.cs
+++ Graphic-Renderer/Graphic-Renderer/Pong/ball.cs
@@ -11,20 +11,51 @@ namespace Graphic_Renderer
             painter = painterInp;
         }
 
+        string ballTexture = @"C:\Users\Neo\Source\Repos\HerrDextro\Lern-Periode-7\Graphic-Renderer\Graphic-Renderer\Pong\textures\ball.txt";
+
         SPainter painter;
 
-        int xpos;
-        int ypos;
+        int xpos = 30; //middle fo filed is x 30 or x 29 or smt
+        int ypos = 15; //middle of field is y15
+        //PLS DONOUT RESET IN RENDER YOU DAMABSS
+        int xspeed = 1;
+        int yspeed = 1;
 
         public void render()
         {
-            painter.changePixel("darkCyan", xpos, ypos);
-            ypos--;
+            painter.loadImage(xpos, ypos, ballTexture);
+
+            //ballmovement stuff plus constaint
+            //int xposB = 30; //center of field
+            //int yposB = 22; //center of field (I hope)
+
+            xpos += xspeed;
+            ypos += yspeed;
+
+            if (xpos <= 0)
+            {
+                yspeed *= -1;
+            }
+            if (ypos <= 0)
+            {
+                yspeed *= -1;
+            }
+            if (xpos >= 58)
+            {
+                xspeed *= -1;
+            }
+            if (ypos >= 29)
+            {
+                yspeed *= -1;
         }
 
         public bool stillExists()
         {
             return ypos >= 0;
         }
+
+        
+
+        
     }
 }
