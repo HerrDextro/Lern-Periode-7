c4581d965e85cac3e0d7c55f45e398ef27ac03d5
Neoow <142605719+HerrDextro@users.noreply.github.com>
2024-11-29 09:02:10 +0100

    Add files via upload
    startup animation file for main branch

---
+++ Graphic-Renderer/Graphic-Renderer/StartUp.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.Design;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Graphic_Renderer
+{
+    public class StartUp
+    {
+        SPainter painter;
+        StartUp startUp = new StartUp();    
+
+        public static bool StartUpAnim(SPainter painter)
+        {
+            bool startUpAnimComplete = false; //set this to true only when start up animation is completed
+            
+
+            //INSERT STARTUP ANIMATIONS HERE
+            painter.fillRectangle("darkred", 0, 0, 60, 30);
+            
+            Thread.Sleep(2000); //dictates how long startup anim will be 
+            startUpAnimComplete = true;
+            return startUpAnimComplete; //returns true for StartUpAnim completed
+          
+      
+
+
+
+
+
+        }
+    }
+}
