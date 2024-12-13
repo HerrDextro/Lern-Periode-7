3ee6217ae906839035729c72787563b3adb85927
Keinstein0 <alex@see5.ch>
2024-12-13 10:21:02 +0100

    nothing

--- Graphic-Renderer/Graphic-Renderer/obj/Debug/net8.0/Graphic-Renderer.AssemblyInfo.cs
+++ Graphic-Renderer/Graphic-Renderer/obj/Debug/net8.0/Graphic-Renderer.AssemblyInfo.cs
@@ -3,7 +3,7 @@
 //     Dieser Code wurde von einem Tool generiert.
 //     Laufzeitversion:4.0.30319.42000
 //
-//     Änderungen an dieser Datei können falsches Verhalten verursachen und gehen verloren, wenn
+//     �nderungen an dieser Datei k�nnen falsches Verhalten verursachen und gehen verloren, wenn
 //     der Code erneut generiert wird.
 // </auto-generated>
 //------------------------------------------------------------------------------
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc34f4c631a673863abf5bbe15af0e2c54fd27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92b1c3f98d46e86cd68246859148ca2771354ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
