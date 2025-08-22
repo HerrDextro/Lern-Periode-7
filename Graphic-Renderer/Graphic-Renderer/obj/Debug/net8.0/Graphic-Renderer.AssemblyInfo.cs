ccc58304f9944d9dbc41394b35c5da05886cf519
Neoow <142605719+HerrDextro@users.noreply.github.com>
2025-08-22 17:40:28 +0200

    after pull

--- Graphic-Renderer/Graphic-Renderer/obj/Debug/net8.0/Graphic-Renderer.AssemblyInfo.cs
+++ Graphic-Renderer/Graphic-Renderer/obj/Debug/net8.0/Graphic-Renderer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb869f48a2b2ab20fb8a28ff523e95bef28d445")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9217a3bb199248dddbc6e7bfb3272ca1eef3505d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graphic-Renderer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
